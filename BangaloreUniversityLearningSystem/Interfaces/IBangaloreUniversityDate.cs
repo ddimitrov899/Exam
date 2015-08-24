0cae25f5c44632b8d0adfa69c2d4e906a1144a55
Димитър Димитров <d.dimitrov899@hotmail.com>
2015-08-24 16:49:53 +0300

    Add using in a namespace

--- BangaloreUniversityLearningSystem/Interfaces/IBangaloreUniversityDate.cs
+++ BangaloreUniversityLearningSystem/Interfaces/IBangaloreUniversityDate.cs
@@ -1,11 +1,12 @@
-﻿using BangaloreUniversityLearningSystem.Data;
-using BangaloreUniversityLearningSystem.Utilities;
-
-namespace BangaloreUniversityLearningSystem.Interfaces
+﻿namespace BangaloreUniversityLearningSystem.Interfaces
 {
+    using Data;
+    using Utilities;
+
     public interface IBangaloreUniversityDate
     {
         UsersRepository Users { get; }
+
         IRepository<Course> Courses { get; }
     }
 }
