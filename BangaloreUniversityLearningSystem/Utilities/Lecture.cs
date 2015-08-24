08536f77a9796549d6e3e59d99d4db1afc7ebbf2
Димитър Димитров <d.dimitrov899@hotmail.com>
2015-08-24 15:08:55 +0300

    Add Lecture.cs and Details.cs

---
+++ BangaloreUniversityLearningSystem/Utilities/Lecture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BangaloreUniversityLearningSystem.Utilities
+{
+    public class Lecture
+    {
+        private string _name;
+
+        public Lecture(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                if (value == null || value.Length < 3)
+                    throw new ArgumentException(string.Format("The lecture name must be at least 3 symbols long."));
+                this._name = value;
+            }
+        }
+    }
+}
---
+++ BangaloreUniversityLearningSystem/Views/Courses/Details.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Text;
+using BangaloreUniversityLearningSystem.Infrastructure;
+using BangaloreUniversityLearningSystem.Utilities;
+
+namespace BangaloreUniversityLearningSystem.Views.Courses
+{
+    public class Details : View
+    {
+        public Details(Course course)
+            : base(course)
+        {
+        }
+
+        internal override void BuildViewResult(StringBuilder viewResult)
+        {
+            var course = this.Model as Course;
+            viewResult.AppendLine(course.Name);
+            if (!course.Lectures.Any())
+            {
+                viewResult.AppendLine("No lectures");
+            }
+            else
+            {
+                var lectureNames = course.Lectures.Select(l => "- " + l.Name);
+                viewResult.AppendLine(string.Join(Environment.NewLine, lectureNames));
+            }
+        }
+    }
+}
+
